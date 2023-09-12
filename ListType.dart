2f819a7b4366770d644926d87af97b2255902b67
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-09-12 10:07:59 +0530

    List Type

---
+++ ListType.dart
@@ -0,0 +1,26 @@
+void main() {
+  
+  List <int> first=[1,2,3,4,5];
+  List second=['aa','bb','cc'];
+  second.add('dd');
+  second.add(123);
+  second.remove('aa');
+  print(second);
+  
+  print('');
+  print('For Loop');
+  print('');
+  for(int i=0;i<second.length;i++)
+  {
+    print(second[i]);
+  }
+  print('');
+  print('For Each Loop');
+  print('');
+  
+  for(var i in second)
+  {
+     print(i);
+  }
+  
+}
