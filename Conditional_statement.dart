6b137eba698c7ef79491319812fdaed9f8547e6e
Rajendra Bhausaheb Pacharne <69509779+rajendrapacharne@users.noreply.github.com>
2023-09-12 14:15:24 +0530

    if else

--- Conditional_statement.dart
+++ Conditional_statement.dart
@@ -1,18 +1,31 @@
 void main()
 {
-   switch(0)
-   {
-    case 5:
-    print('5');
-    break;
+  //  switch(0)
+  //  {
+  //   case 5:
+  //   print('5');
+  //   break;
 
-    case 4:
-    print('4');
-    break;
+  //   case 4:
+  //   print('4');
+  //   break;
 
-    default:
-    print('Invalid Number ');
-    break;
+  //   default:
+  //   print('Invalid Number ');
+  //   break;
 
-   }
+  //  }
+
+
+  int a=10;
+  int b=20;
+  if(a>b)
+  {
+    print("a is greater");
+  }
+  else{
+    print("b is greater");
+  }
+
+
 }
